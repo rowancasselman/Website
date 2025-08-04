7bd9c98012f8be06a1a77eba3607049793b9d9a4
Rowan Casselman <rowancasselman@Rowans-MacBook-Pro.local>
2025-08-04 13:28:25 -0400

    Describe what you changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import PlotGrid from './components/PlotGrid';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col">
-      <div className="flex-1 overflow-auto">
-        <div className="flex flex-wrap items-start justify-center w-full h-full">
-          <PlotGrid />
-        </div>
+    <div
+      className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4"
+      style={{ WebkitOverflowScrolling: "touch" }}
+    >
+      <div className="w-full max-w-screen-md overflow-x-auto">
+        <PlotGrid />
       </div>
       <Toaster position="bottom-center" />
     </div>
