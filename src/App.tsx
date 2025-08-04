06b4d51b70e318e53cfde604d6b0ca526d1c8be1
Rowan Casselman <rowancasselman@Rowans-MacBook-Pro.local>
2025-08-04 13:32:20 -0400

    Describe what you changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import PlotGrid from './components/PlotGrid';
 
 export default function App() {
   return (
-    <div
-      className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4"
-      style={{ WebkitOverflowScrolling: "touch" }}
-    >
-      <div className="w-full max-w-screen-md overflow-x-auto">
-        <PlotGrid />
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center p-4">
+      {/* Scrollable horizontal wrapper */}
+      <div className="w-full max-w-full overflow-x-auto">
+        {/* Grid should size itself naturally */}
+        <div className="inline-block">
+          <PlotGrid />
+        </div>
       </div>
       <Toaster position="bottom-center" />
     </div>
